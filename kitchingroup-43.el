a6af00c9d91ff73dc9c945084b2467dc991b4ae3
John Kitchin <jkitchin@andrew.cmu.edu>
2016-01-31 18:02:22 -0500

    add better headers

--- kitchingroup-43.el
+++ kitchingroup-43.el
@@ -1,23 +1,33 @@
-;;; kitchingroup-43.el --- preprint doi:10.1007/s11244-013-0166-3  -*- lexical-binding: t; -*-
+;;; kitchingroup-43.el --- preprint doi:10.1007/s11244-013-0166-3
 
 ;; Copyright (C) 2016  John Kitchin
 ;; Version: 1
-;; Author: John Kitchin <jkitchin@andrew.cmu.edu>
+;; Author: Spencer D. Miller
+;;  Vladimir V. Pushkarev
+;;  Andrew J. Gellman
+;;  John R Kitchin <jkitchin@andrew.cmu.edu>
 ;; Keywords:
 ;; Package-Requires: ((cappa "0"))
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; DOI: 10.1007/s11244-013-0166-3
+;; Journal: Topics in Catalysis
+;; Year: 2014
+;; URL: http://link.springer.com/article/10.1007%2Fs11244-013-0166-3
+;; Bibtex: ;; @article{miller-2014-simul-temper,
+;;   author =	 {Spencer D. Miller and Vladimir V. Pushkarev and Andrew
+;;                   J. Gellman and John R. Kitchin},
+;;   title =	 {Simulating Temperature Programmed Desorption of Oxygen on
+;;                   {P}t(111) Using {DFT} Derived Coverage Dependent Desorption
+;;                   Barriers},
+;;   journal =	 {Topics in Catalysis},
+;;   volume =	 57,
+;;   number =	 {1-4},
+;;   pages =	 {106-117},
+;;   year =	 2014,
+;;   doi =	 {10.1007/s11244-013-0166-3},
+;;   url =	 {http://dx.doi.org/10.1007/s11244-013-0166-3},
+;;   keywords =	 {DESC0004031, early-career, orgmode, },
+;;  }
+;;
 
 ;;; Commentary:
 
@@ -37,6 +47,7 @@
 ;;   url =		 {http://dx.doi.org/10.1007/s11244-013-0166-3},
 ;;   keywords =	 {DESC0004031, early-career, orgmode, },
 ;; }
+
 
 ;;; Code:
 (require 'cappa)
